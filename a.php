7f2ba918f774fcb1bbced92da024abf1b697dbce
pornthida-sroisena <66010914077@msu.ac.th>
2026-02-10 09:56:39 +0700

    Add files via upload

---
+++ a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>พรธิดา สร้อยเสนา(กิ๊ก)</title>
+</head>
+
+<body>
+<h1>งานที่ 1</h1>
+<h1>พรธิดา สร้อยเสนา(กิ๊ก)</h1>
+</body>
+</html>
